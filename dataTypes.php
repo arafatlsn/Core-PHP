514eab83f2cb0ebc8ce61abf87784ca57c511344
arafatlsn <arafatsarkar22@gmail.com>
2025-07-07 08:49:39 +0600

    beginning with php

---
+++ dataTypes.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - DATA TYPES</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="px-[1rem] py-[.5rem]">
+    <?php
+    $myArray = ['arafat', 25, '5ft 6ichs'];
+    echo "<strong class='text-red-500'>There are these types of data types in PHP.</strong>";
+    print_r($myArray[0]);
+    /* 
+    
+    1. Integer - 333
+    2. Float - 333.33
+    3. String - "Md. Arafat Hossan Lisan"
+    4. Boolean - true/false
+    5. Array - ["Md. Arafat Hossan Lisan, 25, "5ft 6inchs"]
+    6. Object -
+    7. null
+    */
+    ?>
+</body>
+</html>
